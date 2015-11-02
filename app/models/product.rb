c96e053a5ad11f4a8bdef553700870280e37e709
ArunKumarJainK <arunjain@freshdesk.com>
2015-11-02 15:55:29 +0530

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ActiveRecord::Base
+	validates :price, :title, :description, :image_url, presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	validates :title, uniqueness: true
+	validates :image_url, format: 
+						{
+							with: %r{([^\s]+(\.(?i)(jpg|png|gif|bmp))$)},
+							message: 'must be a URL ofr GIF, JPG or PNG image.'
+						}
 end
