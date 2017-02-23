eecb39ed18e677d9083c3c153c8583f34b5aae4c
Arun Kumar Jain K <arunjain@freshdesk.com>
2017-02-23 22:00:28 +0530

    URL Changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   		session[:user_id] = user.id
   		redirect_to admin_url
   	else
-  		redirect_to login_url, alert: "Invalid user/password configuration"
+  		redirect_to store_url, alert: "Invalid user/password configuration"
   	end
   end
 
